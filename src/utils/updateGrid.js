210759ed7ea123c5a15a254200773670068d723b
DanilsGit <danielestebanmarquez12@gmail.com>
2024-06-15 23:27:51 -0500

    add lucky feature

--- src/utils/updateGrid.js
+++ src/utils/updateGrid.js
@@ -34,18 +34,22 @@ const getNeighbors = (grid, x, y) => {
   Reglas de transición:
   1. Si una zona rica no tiene al menos dos vecino ricos, su avg income se reduce un 20%, si no tiene al menos 1 un 30%
   2. Si la zona es pobre y el ingreso promedio de los vecinos es mayor que 0.6, entonces el nivel de educación de la celda actual se establece en 0.65 para convertir la celda pobre en clase media
-  3. Si una zona clase media tiene 3 zonas ricas vecinas, su promedio de ingresos aumenta un +10% en cada iteración, si es clase baja un +6%
+  3. Si una zona clase media tiene 3 zonas ricas vecinas, su promedio de ingresos aumenta un +5% en cada iteración, si es clase baja un +1%
 
   Otras reglas de transición:
   4. si una zona rica esta solo rodeada por vecinos pobres se convierte en media (pierde el 50% de ingresos y patrimonio)
+  5. Factor suerte: En cada iteración se determina en un 50% de aumentar los ingresos de la célula o en un 30% de disminuirlos
 
   Políticas
-  1. política timidez social-demócrata => impuestos a los ricos => Educación en sectores pobres y servicios públicos => convierte la mayoría de pobres en clase media, ricos pierden el 30% de su income
+  1. política timidez social-demócrata => impuestos a los ricos =>
+    Educación en sectores pobres y servicios públicos =>
+    convierte la mayoría de pobres en clase media
+    Los ricos pierden un porcentaje de su ingreso que depende de su nivel de ingreso
   2. política neoliberal => Aumenta el avg_income de los ricos, desplaza algún porcentaje de la clase media a la pobreza
   3. política socialismo democrático => avg income de los vecinos se se reparte en entre todos los vecinos igualitariamente y aumentan en +20% los servicios y educación (por cada iteración)
   */
 
-  // Función que actualiza el estado de una celda
+// Función que actualiza el estado de una celda
 const updateCellState = (cell, neighbors, policy) => {
   // Obtener los inputs de las celdas vecinas
   const cellInputs = getCellInputs(neighbors);
@@ -79,13 +83,13 @@ const updateCellState = (cell, neighbors, policy) => {
     newEducation = 0.65; // Convierte pobre en clase media si vecinos tienen alto ingreso
   }
 
-  //3. Si una zona clase media tiene 3 zonas ricas vecinas, su promedio de ingresos aumenta un +10% en cada iteración, si es clase baja un +6%
+  //3. Si una zona clase media tiene 3 zonas ricas vecinas, su promedio de ingresos aumenta un +5% en cada iteración, si es clase baja un +1%
   if (richNeighbors >= 3) {
     //El tope se asegura que no se convierta en rico
     if (cell.income_level === "Medium" && cell.avg_income < 2) {
-      newIncome *= 1.1; // Clase media con 3 vecinos ricos aumenta 10%
+      newIncome *= 1.1; // Clase media con 3 vecinos ricos aumenta 5%
     } else if (cell.income_level === "Low" && cell.avg_income < 2) {
-      newIncome *= 1.06; // Clase baja con 3 vecinos ricos aumenta 6%
+      newIncome *= 1.01; // Clase baja con 3 vecinos ricos aumenta 1%
     }
   }
 
@@ -104,21 +108,34 @@ const updateCellState = (cell, neighbors, policy) => {
     };
   }
 
+  // 5. Factor suerte: En cada iteración se determina en un 50% de aumentar los ingresos de la célula o en un 30% de disminuirlos
+  // numero de 1 a 100
+  const luckyNumber = Math.floor(Math.random() * 100) + 1;
+  if (luckyNumber <= 50) {
+    // Aumentarle el 1% de su ingreso
+    newIncome += 0.01 * newIncome;
+  }
+  else if (luckyNumber <= 80) {
+    // Disminuir el 1% de su ingreso
+    newIncome -= 0.01 * newIncome;
+  }
+
+
   // Aplicar políticas
   switch (policy) {
     //1. Política de redistribución social demócrata
-    // impuestos a los ricos => 
-    // Educación en sectores pobres y servicios públicos => convierte la mayoría de pobres en clase media
-    // ricos pierden el 30% de su income
+    // Los ricos pierden un porcentaje de su ingreso.
+    // Este porcentaje depende de su nivel de ingreso promedio (avg_income).
     case 1:
-      if (cell.income_level === "High" && cell.avg_income > 10) { //Si es rico y tiene ingreso mayor a 10
-        newIncome *= 0.7; // Ricos pierden 30% del ingreso
+      if (cell.income_level === "High") { //Si es rico
+        // Los ricos pierden un porcentaje de su ingreso que depende de su nivel de ingreso
+        newIncome *= 1 - 0.3 * (cell.avg_income/25);
       } else if (cell.income_level === "Low") { //Si es pobre
         newEducation = Math.min(newEducation + 0.2, 0.8); // Mejora educación. 
         newServices = {
-          schools: Math.min(newServices.schools + 0.2, 0.8), // Mejora servicios educativos
-          hospitals: Math.min(newServices.hospitals + 0.2, 0.8), // Mejora servicios de salud
-          public_transport: Math.min(newServices.public_transport + 0.2, 0.8), // Mejora transporte público
+          schools: newServices.schools <= 0.5 ? Math.min(newServices.schools + 0.2, 0.8) : newServices.schools, // Si no pasa de 0.8 mejora servicios educativos
+          hospitals: newServices.hospitals <= 0.5 ? Math.min(newServices.hospitals + 0.2, 0.8) : newServices.hospitals, // Si no pasa de 0.8 mejora servicios de salud
+          public_transport: newServices.public_transport <= 0.5 ? Math.min(newServices.public_transport + 0.2, 0.8) : newServices.public_transport, // Si no pasa de 0.8 mejora transporte público
         }; // Mejora servicios públicos
       }
       break;
@@ -133,7 +150,7 @@ const updateCellState = (cell, neighbors, policy) => {
       break;
     // Política socialista democrática
     // => avg income de los vecinos se se reparte en entre todos los vecinos igualitariamente y aumentan en +20% los servicios y educación (por cada iteración)
-    case 3: 
+    case 3:
       const equalIncome =
         (totalIncome + cell.avg_income) / (neighbors.length + 1); // Redistribución igualitaria del ingreso
       newIncome = equalIncome;
